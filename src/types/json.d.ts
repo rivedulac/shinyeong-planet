d2ba1726c71c829f1105c5d7edaaab395076d6bd
rivedulac <shinyeong.dev@gmail.com>
2025-03-14 22:44:01 +0900

    Updates

--- src/types/json.d.ts
+++
@@ -1,5 +0,0 @@
-// Allows importing JSON files directly in TypeScript
-declare module "*.json" {
-  const value: any;
-  export default value;
-}
--- src/ui/virtualControls/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all virtual control components from this index file
-export { default as VirtualControlsToggle } from "./VirtualControlsToggle";
-export { default as VirtualMoveControls } from "./VirtualMoveControls";
-export { default as VirtualRotationControls } from "./VirtualRotationControls";
-export { default as VirtualZoomControls } from "./VirtualZoomControls";
