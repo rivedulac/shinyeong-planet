e13f233d670d7a19f384b3cbbfdfa511f25a7967
rivedulac <shinyeong.dev@gmail.com>
2025-03-26 16:38:56 +0900

    Increase the planet radius
    Changelog
    ======
    - Increased the planet radius from 25 to 30
    
    Testing
    ======
    - `npm run dev`
    - `npm test`
    - `npm run build`

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
 import { IConversation } from "@/game/npcs/interfaces/IConversation";
 import * as THREE from "three";
 
-export const PLANET_RADIUS = 25;
+export const PLANET_RADIUS = 30;
 export const BACKGROUND_RADIUS = 700;
 export const FIRST_PERSON_HEIGHT = 5;
 export const PLANET_CENTER = new THREE.Vector3(0, 0, 0);
